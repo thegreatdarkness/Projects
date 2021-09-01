7020af41e3f6578574f1f20f1c2aa93e7523e50a
Maciej Szczepaniak <czarnadzuma11@gmail.com>
2021-09-02 00:26:10 +0200

    Fix for object reinitialized after each operation.
    Suggested improvements
    Code style fixes
    
    Signed-off-by: Maciej Szczepaniak <czarnadzuma11@gmail.com>

--- product_inventory.py
+++ product_inventory.py
@@ -1,9 +1,10 @@
-#  Create an application which manages an inventory of products. Create a product class which has a price, id, and
-#  quantity on hand. Then create an inventory class which keeps track of various products and can sum up the inventory
-#  value.
+#  Create an application which manages an inventory of products.
+#  Create a product class which has a price, id, and
+#  quantity on hand. Then create an inventory class which keeps track
+#  of various products and can sum up the inventory value.
+
 
 class Product:
-
     def __init__(self, prod_id, price, quantity):
         self.prod_id = prod_id
         self.price = price
@@ -30,73 +31,89 @@ class Product:
         return self.quantity
 
     def view_product(self):
-        print("Product ID: " + str(self.prod_id) + ", Price: " + str(self.price) + ", Quantity: " + str(self.quantity))
+        print("Product ID: " +
+              str(self.prod_id) +
+              ", Price: " + str(self.price) +
+              ", Quantity: " +
+              str(self.quantity))
 
 
 class Inventory:
-
     list_prod = None
+    action = None
 
-    def __init__(self):
-        self.list_prod = {"Name": [], "Price": [], "Quantity": []}
-        self.action = input("Options: 1 - Add product, 2 - Delete product, 3 - View inventory, 4 - Exit: ")
+    def __init__(self, initial_inventory=[]):
+        self.list_prod = initial_inventory
 
     def __repr__(self):
         return f"{self.list_prod}"
-
-    def __eq__(self, other):
-        return True if self.list_prod["Name"] == other.prod_id else False
 
     def add_product(self):
         name = str(input("Input product name: "))
         price = float(input("Input price: "))
         quantity = int(input("Input quantity in stock: "))
-        self.list_prod["Name"].append(name)
-        self.list_prod["Price"].append(price)
-        self.list_prod["Quantity"].append(quantity)
+
+        self_list_prod.append(
+                                {
+                                    "Name": name,
+                                    "Price": price,
+                                    "Quantity": quantity
+                                }
+                            )
         print(self.list_prod)
 
     def remove_prod(self):
-        rmv_prod_id = str(input("Which product should be removed?: "))
+        rmv_product_name = str(input("Name of product to remove: "))
 
-        def get_position():
-            return self.list_prod["Name"].index(rmv_prod_id)
-
-        if rmv_prod_id in self.list_prod["Name"]:
-            position = get_position()
-            name = self.list_prod["Name"]
-            price = self.list_prod["Price"]
-            quantity = self.list_prod["Quantity"]
-            self.list_prod["Name"].remove(name[position])
-            self.list_prod["Price"].remove(price[position])
-            self.list_prod["Quantity"].remove(quantity[position])
-
+        for product in self.list_prod:
+            if product['Name'] == rmv_product_name:
+                product_to_remove = product
+                break
         else:
             print("Error. Product not in the inventory.")
+            return
+
+        self.list_prod.remove(product_to_remove)
+
 
     def view_inventory(self):
-        print(self.list_prod)
-
-    # if __name__ == '__main__':
-
-    def __call__(self, *args, **kwargs):
-        x = int(self.action)
-        if True:
-            if x == 1:
-                self.add_product()
-                self.__init__()
-                self.__call__()
-            if x == 2:
-                self.remove_prod()
-                self.__init__()
-                self.__call__()
-            if x == 3:
-                self.view_inventory()
-                self.__init__()
-                self.__call__()
-            while self.action == 4:
-                break
+        for prod in self.list_prod:
+            print(prod)
 
 
-invent1 = Inventory()
-invent1()
+def interactive_manage(inv_instance):
+    while True:
+        action = input("Options: 1 - Add product,"
+                       "2 - Delete product, "
+                       "3 - View inventory, "
+                       "4 - Exit: ")
+        if action == '1':
+            inv_instance.add_product()
+        elif action == '2':
+            inv_instance.remove_prod()
+        elif action == '3':
+            inv_instance.view_inventory()
+        elif action == '4':
+            break
+        else:
+            print('Option invalid')
+
+
+def main():
+    initial_inventory = [{
+                            'Name': 'sandau',
+                            'Price': 10,
+                            'Quantity': 3
+                        },
+                        {
+                            'Name': 'kozak',
+                            'Price': 20,
+                            'Quantity': 4
+                        }]
+
+    shoe_inventory = Inventory(initial_inventory)
+    interactive_manage(shoe_inventory)
+
+
+if __name__ == '__main__':
+    main()
