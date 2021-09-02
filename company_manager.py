87032a3bed1cf957a0b5e25faf0e1b5b7693afdb
thegreatdarkness <mcvagene@gmail.com>
2021-09-02 16:51:46 +0200

    Upgraded hire_emp and added view_emps

--- company_manager.py
+++ company_manager.py
@@ -1,7 +1,8 @@
-# Create an hierarchy of classes - abstract class Employee and subclasses HourlyEmployee, SalariedEmployee, Manager
-# and Executive. Every one's pay is calculated differently, research a bit about it. After you've established an
-# employee hierarchy, create a Company class that allows you to manage the employees. You should be able to hire,
-# fire and raise employees.
+# Create an hierarchy of classes - abstract class Employee and subclasses
+# HourlyEmployee, SalariedEmployee, Manager and Executive. Every one's pay
+# is calculated differently, research a bit about it. After you've established
+# an employee hierarchy, create a Company class that allows you to manage the
+# employees. You should be able to hire, fire and raise employees.
 
 class Employee:
 
@@ -15,10 +16,7 @@ class Employee:
 
 
 class HourlyEmployee(Employee):
-    hourly_emps = {
-        "Name": [],
-        "Pay": [],
-    }
+    hourly_emps = [{'Name': 'John White', 'Pay': 1440.0}, ]
 
     def __init__(self, first, last, wage, num_hours):
         super().__init__(first, last)
@@ -29,15 +27,9 @@ class HourlyEmployee(Employee):
         pay = float(self.wage * self.num_hours)
         return pay
 
-    def show_lst(self):
-        return HourlyEmployee.hourly_emps
-
 
 class SalariedEmployee(Employee):
-    salaried_emps = {
-        "Name": [],
-        "Pay": [],
-    }
+    salaried_emps = [{'Name': 'Alice Sam', 'Pay': 2000.0}, ]
 
     def __init__(self, first, last, salary, num_period):
         super().__init__(first, last)
@@ -48,86 +40,60 @@ class SalariedEmployee(Employee):
         pay = float(self.salary // self.num_period)
         return pay
 
-    def show_lst(self):
-        return self.salaried_emps
-
 
 class Manager(Employee):
-    managers = {
-        "Name": [],
-        "Pay": [],
-    }
+    managers = [{'Name': 'Kylie Smith', 'Pay': 5000}, ]
+    extra = None
 
-    def __init__(self, first, last, base, extra):
+    def __init__(self, first, last, base):
         super().__init__(first, last)
         self.base = base
-        self.extra = extra
 
     def calc_extra(self):
-        extra = float(self.base * self.extra)
-        return extra
+        self.extra = float(self.base * 0.10)
+        return self.extra
 
     def calc_pay(self):
         pay = float(self.base + self.extra)
         return pay
 
-    def show_lst(self):
-        return self.managers
-
 
 class Executive(Employee):
-    executives = {
-        "Name": [],
-        "Pay": [],
-    }
+    executives = [{'Name': 'Ping Zen', 'Pay': 10000}, ]
+    extra = None
+    bonus = None
 
-    def __init__(self, first, last, base, extra, bonus):
+    def __init__(self, first, last, base):
         super().__init__(first, last)
         self.base = base
-        self.extra = extra
-        self.bonus = bonus
 
     def calc_extra(self):
-        extra = float(self.base * self.extra)
-        return extra
+        self.extra = float(self.base * 0.20)
+        return self.extra
 
     def calc_bonus(self):
-        bonus = float(0.01 * Company.company_income)
-        return bonus
+        self.bonus = float(0.01 * Company.company_income)
+        return self.bonus
 
     def calc_pay(self):
         pay = float(self.base + self.extra + self.bonus)
         return pay
-
-    def show_lst(self):
-        return Executive.executives
 
 
 class Company:
 
     company_income = 1000000
 
-    emps = {
-        "Hourly": HourlyEmployee.hourly_emps,
-        "Salaried": SalariedEmployee.salaried_emps,
-        "Managers": Manager.managers,
-        "Executives": Executive.executives
-    }
+    emps = None
+    action = None
 
     def __init__(self):
-        action = int(input("Options: 1 - Hire, 2 - Fire, 3 - Give raise, 4 - View, 5 - Exit: "))
-        if action == 1:
-            self.hire_emp()
-        elif action == 2:
-            self.fire_emp()
-        elif action == 3:
-            self.give_raise()
-        elif action == 4:
-            print(Company.emps)
-        while action == 5:
-            break
-        else:
-            print("Option not valid.")
+        self.emps = {
+                     "Hourly": HourlyEmployee.hourly_emps,
+                     "Salaried": SalariedEmployee.salaried_emps,
+                     "Managers": Manager.managers,
+                     "Executives": Executive.executives
+                    }
 
     def hire_emp(self):
         emp_class = input("Enter Employee class (H/S/M/E): ")
@@ -136,53 +102,125 @@ class Company:
             HourlyEmployee.last = str(input("Enter last name: "))
             HourlyEmployee.wage = int(input("Enter wage: "))
             HourlyEmployee.num_hours = int(input("Enter number of hours: "))
-            emp = HourlyEmployee(HourlyEmployee.first, HourlyEmployee.last, HourlyEmployee.wage,
-                                 HourlyEmployee.num_hours)
+            emp = HourlyEmployee(HourlyEmployee.first, HourlyEmployee.last,
+                                 HourlyEmployee.wage, HourlyEmployee.num_hours)
             fullname = HourlyEmployee.fullname(emp)
             pay = HourlyEmployee.calc_pay(emp)
-            HourlyEmployee.hourly_emps["Name"].append(fullname)
-            HourlyEmployee.hourly_emps["Pay"].append(pay)
+            HourlyEmployee.hourly_emps.append({"Name": fullname, "Pay": pay})
+            print(fullname, "was added as Hourly employee")
         elif emp_class == 'S':
             SalariedEmployee.first = str(input("Enter first name: "))
             SalariedEmployee.last = str(input("Enter last name: "))
             SalariedEmployee.salary = int(input("Enter salary: "))
             SalariedEmployee.num_periods = int(input("Enter number of periods: "))
-            emp = SalariedEmployee(SalariedEmployee.first, SalariedEmployee.last, SalariedEmployee.salary,
-                                   SalariedEmployee.num_periods)
+            emp = SalariedEmployee(SalariedEmployee.first, SalariedEmployee.last,
+                                   SalariedEmployee.salary, SalariedEmployee.num_periods)
             fullname = SalariedEmployee.fullname(emp)
             pay = SalariedEmployee.calc_pay(emp)
-            SalariedEmployee.salaried_emps["Name"].append(fullname)
-            SalariedEmployee.salaried_emps["Pay"].append(pay)
+            SalariedEmployee.salaried_emps.append({"Name": fullname, "Pay": pay})
+            print(fullname, "was added as Salaried employee")
         elif emp_class == 'M':
             Manager.first = str(input("Enter first name: "))
             Manager.last = str(input("Enter last name: "))
             Manager.base = int(input("Enter base: "))
-            Manager.extra = float(input("Enter extra: "))
-            emp = Manager(Manager.first, Manager.last, Manager.base, Manager.extra)
+            emp = Manager(Manager.first, Manager.last, Manager.base)
             fullname = Manager.fullname(emp)
             Manager.calc_extra(emp)
             pay = Manager.calc_pay(emp)
-            Manager.managers["Name"].append(fullname)
-            Manager.managers["Pay"].append(pay)
+            Manager.managers.append({"Name": fullname, "Pay": pay})
+            print(fullname, "was added as Manager")
         elif emp_class == 'E':
-            Executive.first = str(input("Enter first name: "))
-            Executive.last = str(input("Enter last name: "))
-            Executive.base = int(input("Enter base: "))
-            Executive.extra = int(input("Enter extra: "))
-            Executive.bonus = int(input("Enter bonus: "))
-            emp = Executive(Executive.first, Executive.last, Executive.base, Executive.extra, Executive.bonus)
+            Executive.first = input("Enter first name: ")
+            Executive.last = input("Enter last name: ")
+            Executive.base = input("Enter base: ")
+
+            emp = Executive(str(Executive.first), str(Executive.last), int(Executive.base))
             fullname = Executive.fullname(emp)
             Executive.calc_extra(emp)
             Executive.calc_bonus(emp)
             pay = Executive.calc_pay(emp)
-            Executive.executives["Name"].append(fullname)
-            Executive.executives["Pay"].append(pay)
+            Executive.executives.append({"Name": fullname, "Pay": pay})
+            print(fullname, "was added as Executive")
         else:
             print("Employee class not valid.")
         return self.emps
 
     def fire_emp(self):
-        pass
+        emp = input("Employee name to remove:")
+        if emp in self.emps:
+            pass
 
     def give_raise(self):
         pass
+
+    @staticmethod
+    def view_emps():
+        hourly = HourlyEmployee.hourly_emps
+        hourly_lst = []
+        for item in hourly:
+            values = item.get("Name")
+            hourly_lst.append(values)
+        print("Hourly employees:", hourly_lst)
+
+        salaried = SalariedEmployee.salaried_emps
+        salaried_lst = []
+        for item in salaried:
+            values = item.get("Name")
+            salaried_lst.append(values)
+        print("Salaried employees:", salaried_lst)
+
+        managers = Manager.managers
+        managers_lst = []
+        for item in managers:
+            values = item.get("Name")
+            managers_lst.append(values)
+        print("Managers:", managers_lst)
+
+        executives = Executive.executives
+        executives_lst = []
+        for item in executives:
+            values = item.get("Name")
+            executives_lst.append(values)
+        print("Executives:", executives_lst)
+
+    @staticmethod
+    def update_income():
+        company_income = int(input("Update company income: "))
+        return company_income
+
+    @staticmethod
+    def get_details(fullname):
+        while True:
+            for dic in HourlyEmployee.hourly_emps:
+                for atr in dic:
+                if fullname in dic:
+
+
+
+
+def manage(Company):
+    while True:
+        action = input("Options: 1 - Hire, 2 - Fire, 3 - Give raise, 4 - Update income, "
+                       "5 - Employee list, 6 - Get details, 7 - Exit: ")
+        if action == '1':
+            Company.hire_emp()
+        elif action == '2':
+            Company.fire_emp()
+        elif action == '3':
+            Company.give_raise()
+        elif action == '4':
+            Company.update_income()
+        elif action == '5':
+            Company.view_emps()
+        elif action == '7':
+            break
+        else:
+            print("Option not valid.")
+
+
+def main():
+    manage(Company())
+
+
+if __name__ == '__main__':
+    main()
